fa60b0172e6409143ce9b76e9c3d168ea66db763
Danilo Facco <faccodanilo@gmail.com>
2022-03-18 15:57:56 +0000

    theme

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,14 +1,31 @@
+import AuthLogo from './extensions/logo.svg';
+import MenuLogo from './extensions/icon.svg';
+import favicon from './extensions/favicon.ico';
+
 export default {
   config: {
     locales: ['pt-BR'],
+    auth: {
+      logo: AuthLogo,
+    },
+    head: {
+      favicon: favicon,
+    },
   },
   theme: {
     colors: {
-      alternative100: '#f6ecfc',
-      alternative200: '#e0c1f4',
-      alternative500: '#ac73e6',
-      alternative600: '#9736e8',
-      alternative700: '#8312d1',
+      alternative100: '#B4E1F8',
+      alternative200: '#6AC4F1',
+      alternative500: '#138FCE',
+      alternative600: '#1182BB',
+      alternative700: '#0E6895',
+
+      primary100: '#B4E1F8',
+      primary200: '#6AC4F1',
+      primary500: '#138FCE',
+      primary600: '#1182BB',
+      primary700: '#0E6895',
+
       danger700: '#b72b1a'
     },
     // Disable video tutorials
@@ -24,7 +41,7 @@ export default {
         document.querySelector("[href='/admin/list-plugins']").parentElement.remove()
         document.querySelector("[href='/admin/marketplace']").parentElement.remove()
         document.querySelector("[href='/admin/plugins/content-type-builder']").parentElement.remove()
-    }, 1000); 
+    }, 1000);
     });
 
   },
