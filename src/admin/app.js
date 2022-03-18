3135d0911bdc47563cf95ee33f9c47d28401d3b1
Danilo Facco <faccodanilo@gmail.com>
2022-03-18 16:08:17 +0000

    Update app.js

--- src/admin/app.js
+++ src/admin/app.js
@@ -8,25 +8,28 @@ export default {
     auth: {
       logo: AuthLogo,
     },
+    menu: {
+      logo: MenuLogo,
+    },
     head: {
       favicon: favicon,
     },
-  },
-  theme: {
-    colors: {
-      alternative100: '#B4E1F8',
-      alternative200: '#6AC4F1',
-      alternative500: '#138FCE',
-      alternative600: '#1182BB',
-      alternative700: '#0E6895',
+    theme: {
+      colors: {
+        alternative100: '#B4E1F8',
+        alternative200: '#6AC4F1',
+        alternative500: '#138FCE',
+        alternative600: '#1182BB',
+        alternative700: '#0E6895',
 
-      primary100: '#B4E1F8',
-      primary200: '#6AC4F1',
-      primary500: '#138FCE',
-      primary600: '#1182BB',
-      primary700: '#0E6895',
+        primary100: '#B4E1F8',
+        primary200: '#6AC4F1',
+        primary500: '#138FCE',
+        primary600: '#1182BB',
+        primary700: '#0E6895',
 
-      danger700: '#b72b1a'
+        danger700: '#b72b1a'
+      },
     },
     // Disable video tutorials
     tutorials: false,
@@ -41,7 +44,7 @@ export default {
         document.querySelector("[href='/admin/list-plugins']").parentElement.remove()
         document.querySelector("[href='/admin/marketplace']").parentElement.remove()
         document.querySelector("[href='/admin/plugins/content-type-builder']").parentElement.remove()
-    }, 1000);
+      }, 1000);
     });
 
   },
