dee70c2f3a5c81406ab83c6430b84d4667167a87
Danilo Gujansky Facco <faccodanilo@gmail.com>
2022-03-27 22:06:56 +0100

    Update index.js

--- src/plugins/wysiwyg/admin/src/components/Wysiwyg/index.js
+++ src/plugins/wysiwyg/admin/src/components/Wysiwyg/index.js
@@ -10,110 +10,106 @@ import Editor from "../Editor";
 import { useIntl } from "react-intl";
 
 const Wysiwyg = ({
-    name,
-    onChange,
-    value,
-    intlLabel,
-    disabled,
-    error,
-    description,
-    required,
+  name,
+  onChange,
+  value,
+  intlLabel,
+  disabled,
+  error,
+  description,
+  required,
 }) => {
-    const { formatMessage } = useIntl();
-    const [mediaLibVisible, setMediaLibVisible] = useState(false);
+  const { formatMessage } = useIntl();
+  const [mediaLibVisible, setMediaLibVisible] = useState(false);
 
-    const handleToggleMediaLib = () => setMediaLibVisible((prev) => !prev);
+  const handleToggleMediaLib = () => setMediaLibVisible((prev) => !prev);
 
-    const handleChangeAssets = (assets) => {
-        let newValue = value ? value : "";
+  const handleChangeAssets = (assets) => {
+    let newValue = value ? value : "";
 
-        assets.map((asset) => {
-            if (asset.mime.includes("image")) {
-                const imgTag = `<p><img src="${asset.url}" alt="${asset.alt}"></img></p>`;
+    assets.map((asset) => {
+      if (asset.mime.includes("image")) {
+        const imgTag = `<p><img src="${asset.url}" alt="${asset.alt}"></img></p>`;
 
-                newValue = `${newValue}${imgTag}`;
-            }
+        newValue = `${newValue}${imgTag}`;
+      }
 
-            // Handle videos and other type of files by adding some code
-        });
+      // Handle videos and other type of files by adding some code
+    });
 
-        onChange({ target: { name, value: newValue } });
-        handleToggleMediaLib();
-    };
+    onChange({ target: { name, value: newValue } });
+    handleToggleMediaLib();
+  };
 
-    return ( <
+  return (
+    <>
+      <Stack size={1}>
+        <Box>
+          <Typography variant="pi" fontWeight="bold">
+            {formatMessage(intlLabel)}
+          </Typography>
+          {required && (
+            <Typography variant="pi" fontWeight="bold" textColor="danger600">
+              *
+            </Typography>
+          )}
+        </Box>
+        <Button
+          startIcon={<Landscape />}
+          variant="secondary"
+          fullWidth
+          onClick={handleToggleMediaLib}
         >
-        <
-        Stack size = { 1 } >
-        <
-        Box >
-        <
-        Typography variant = "pi"
-        fontWeight = "bold" > { formatMessage(intlLabel) } <
-        /Typography> {
-            required && ( <
-                Typography variant = "pi"
-                fontWeight = "bold"
-                textColor = "danger600" >
-                *
-                <
-                /Typography>
-            )
-        } <
-        /Box> <
-        Button startIcon = { < Landscape / > }
-        variant = "secondary"
-        fullWidth onClick = { handleToggleMediaLib } >
-        Media library <
-        /Button> <
-        Editor disabled = { disabled }
-        name = { name }
-        onChange = { onChange }
-        value = { value }
-        /> {
-            error && ( <
-                Typography variant = "pi"
-                textColor = "danger600" > { formatMessage({ id: error, defaultMessage: error }) } <
-                /Typography>
-            )
-        } {
-            description && ( <
-                Typography variant = "pi" > { formatMessage(description) } < /Typography>
-            )
-        } <
-        /Stack> <
-        MediaLib isOpen = { mediaLibVisible }
-        onChange = { handleChangeAssets }
-        onToggle = { handleToggleMediaLib }
-        /> <
+          Media library
+        </Button>
+        <Editor
+          disabled={disabled}
+          name={name}
+          onChange={onChange}
+          value={value}
         />
-    );
+        {error && (
+          <Typography variant="pi" textColor="danger600">
+            {formatMessage({ id: error, defaultMessage: error })}
+          </Typography>
+        )}
+        {description && (
+          <Typography variant="pi">{formatMessage(description)}</Typography>
+        )}
+      </Stack>
+      <MediaLib
+        isOpen={mediaLibVisible}
+        onChange={handleChangeAssets}
+        onToggle={handleToggleMediaLib}
+      />
+    </>
+  );
 };
 
 Wysiwyg.defaultProps = {
-    description: "",
-    disabled: false,
-    error: undefined,
-    intlLabel: "",
-    required: false,
-    value: "",
+  description: "",
+  disabled: false,
+  error: undefined,
+  intlLabel: "",
+  required: false,
+  value: "",
 };
 
 Wysiwyg.propTypes = {
-    description: PropTypes.shape({
-        id: PropTypes.string,
-        defaultMessage: PropTypes.string,
-    }),
-    disabled: PropTypes.bool,
-    error: PropTypes.string,
-    intlLabel: PropTypes.shape({
-        id: PropTypes.string,
-        defaultMessage: PropTypes.string,
-    }),
-    name: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    required: PropTypes.bool,
-    value: PropTypes.string,
+  description: PropTypes.shape({
+    id: PropTypes.string,
+    defaultMessage: PropTypes.string,
+  }),
+  disabled: PropTypes.bool,
+  error: PropTypes.string,
+  intlLabel: PropTypes.shape({
+    id: PropTypes.string,
+    defaultMessage: PropTypes.string,
+  }),
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  required: PropTypes.bool,
+  value: PropTypes.string,
 };
 
 export default Wysiwyg;
