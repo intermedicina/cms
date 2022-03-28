283d4a2219f289542689680f22e7e049091750be
Danilo Facco <faccodanilo@gmail.com>
2022-03-28 09:15:45 +0100

    RTEditor

--- src/plugins/wysiwyg/admin/src/components/Editor/index.js
+++ src/plugins/wysiwyg/admin/src/components/Editor/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { CKEditor } from "../../../../../../../node_modules/@ckeditor/ckeditor5-react";
+import ClassicEditor from "../../../../../../../node_modules/@ckeditor/ckeditor5-build-classic";
 import { Box } from "@strapi/design-system/Box";
 
 const Wrapper = styled(Box)`
