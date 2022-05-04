2d2d1a951aa09873fe642c0804fec43fa5a0dd91
Danilo Facco <faccodanilo@gmail.com>
2022-05-04 17:33:51 +0100

    pagina produtos

---
+++ src/api/paginas-produto/services/paginas-produto.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * paginas-produto service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::paginas-produto.paginas-produto');
